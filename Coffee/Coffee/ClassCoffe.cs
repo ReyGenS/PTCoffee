2301185e767e2a02520cd136700f943d4c065bae
ReyGenS <DeSh0045@yandex.ru>
2014-03-30 20:31:53 +0400

    coffee

---
+++ Coffee/Coffee/ClassCoffe.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 22.02.2014
+ * Time: 13:55
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Coffee
+{
+	/// <summary>
+	/// Description of ClassCoffe.
+	/// </summary>
+	public class ClassCoffe
+	{
+		public ClassCoffe()
+		{
+			
+		}
+	}
+}
